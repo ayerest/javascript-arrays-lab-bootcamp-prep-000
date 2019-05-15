5a3424b09e3316b5cdfcfbede48113bb541898b4
Iris S <irisiseve@gmail.com>
2019-05-15 03:14:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
-  
+
 }
 
 function destructivelyPrependKitten(name) {
-  
+
 }
 
 function destructivelyRemoveLastKitten() {
